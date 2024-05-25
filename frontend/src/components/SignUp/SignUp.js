27f1a43d34c8740b6b7fc37431a93186008a3d4b
georgiamurariu <georgiamurariu@gmail.com>
2024-05-26 00:21:41 +0300

    signup

--- frontend/src/components/SignUp/SignUp.js
+++ frontend/src/components/SignUp/SignUp.js
@@ -2,19 +2,19 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './SignUp.css';
-
+import SIGNUP from '../../photo/sigup.png';
+import people from '../../photo/runner.png';
 function SignUp() {
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Perform validation or any other operations here
     navigate('/face-recognition');
   };
 
   return (
     <div className="signup">
-      <h2>SignUp</h2>
+      <img src={SIGNUP}/>
       <form className="signup-form" onSubmit={handleSubmit}>
         <label>
           name
@@ -28,17 +28,25 @@ function SignUp() {
           enter password
           <input type="password" name="password" required />
         </label>
-        <label>
-          ID
-          <input type="text" name="id" required />
-        </label>
-        <button type="submit" className="signup-button">NEXT</button>
-      </form>
+        <form action="/your-upload-endpoint" method="post" enctype="multipart/form-data">
+          <label class="file-upload-label">
+            Upload your ID
+            <input type="file" name="id" class="file-upload-input" required />
+          </label>
+       
+        
+    </form>
+      <div>
       <div className="footer">
         {Array(8).fill().map((_, index) => (
-          <span key={index} className="runner">&#x1F3C3;</span>
+          <span key={index} className="runner"><img src={people}/></span>
         ))}
       </div>
+      <button type="submit" className="signup-button">NEXT</button>
+      </div>
+        
+      </form>
+      
       <div className="footer-text">FastPass</div>
     </div>
   );
